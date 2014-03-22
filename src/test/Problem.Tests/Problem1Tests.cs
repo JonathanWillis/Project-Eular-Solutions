2b20b1477fe2bc60d26894dca3862bf1c35486ff
Jonathan Willis <jonathanwillis@codeweavers.net>
2014-03-22 12:59:09 +0000

    Project Setup + Problem 1

---
+++ src/test/Problem.Tests/Problem1Tests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace Problem.Tests
+{
+    [TestFixture]
+    public class Problem1Tests
+    {
+        [Test]
+        public void Problem1Sample()
+        {
+            int result = Problem1.Solve(10);
+            Assert.That(result, Is.EqualTo(23));
+        }
+
+        [Test]
+        public void Problem1Solution()
+        {
+            int result = Problem1.Solve(1000);
+            Assert.That(result, Is.EqualTo(233168));
+        }
+    }
+}
